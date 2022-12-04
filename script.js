8041b0a509e35969e8b71a3cdff70e1b2ac8c06f
AliHussein <alihusseiinali@gmail.com>
2022-12-04 19:50:22 +0200

    dice,currentScire and currentplayer functionality added

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+const scoreP0E = document.getElementById("score--0");
+const scoreP1E = document.getElementById("score--1");
+const currentScore0E = document.getElementById("current--0");
+const currentScore1E = document.getElementById("current--1");
+const diceE = document.querySelector(".dice");
+const btnRoll = document.querySelector(".btn--roll");
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+
+let currentScore = 0;
+// conditions
+scoreP0E.textContent = 0;
+scoreP1E.textContent = 0;
+diceE.classList.add("hidden");
+
+// rolling the dice functionality
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceE.classList.remove("hidden");
+  diceE.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    currentScore0E.textContent = currentScore;
+  }
+  // else {
+
+  // }
+});
